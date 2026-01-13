4e56553383312a4d997fb6774fa1b6609beea6ff
DhannV <alrizky2906@gmail.com>
2026-01-14 00:20:55 +0700

    googleAuth

--- backend/src/services/googleAuth.js
+++ backend/src/services/googleAuth.js
@@ -15,7 +15,7 @@ passport.use(
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       callbackURL:
-        "https://2nd-cafemydurian.vercel.app/api/auth/google/callback",
+        "https://twond-cafemydurian.onrender.com/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
